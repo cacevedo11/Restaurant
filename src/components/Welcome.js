f09ee944e21c18153189aa26fdceaffe14b2c4ab
Christian Acevedo <cacesa11@gmail.com>
2022-10-27 00:34:21 -0500

    cambio3

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -6,7 +6,7 @@ import Bar from '../Images/bar.jpg'
 
 function Welcome() {
   return (
-  <div className='bg-slate-800 min-h-screen flex flex-col  gap-6'>
+  <div className='bg-slate-800 min-h-screen flex flex-col  gap-6 pt-[20%]  lg:pt-[5%]'>
 
       <div className=' h-40 text-3xl font-semibold p-10 text-center text-white font-poppins lg:text-5xl md:text-4xl sm:text-4xl'>
       <Typewriter
@@ -20,12 +20,12 @@ function Welcome() {
  
     <div className='items-center justify-center grid grid-cols-1 md:grid-cols-2 gap-12  '>
 
-        <div className='flex flex-col items-center m-auto w-3/4 rounded-lg '>
+        <div className='flex flex-col items-center m-auto w-3/4 rounded-lg lg:w-[60%] '>
           <img className='rounded-lg' src={Restaurant} alt=''/>
             <h3 className='my-[0] text-white font-poppins text-2xl font-medium'>Restaurante</h3>
         </div>
 
-        <div className='flex flex-col items-center m-auto w-3/4 rounded-lg' >
+        <div className='flex flex-col items-center m-auto w-3/4 rounded-lg lg:w-[60%]' >
           <img className='rounded-lg' src={Bar} alt=''/>
           <h3 className='my-[0] text-white font-poppins text-2xl  font-medium z-10'>Bar</h3>
         </div>
