492b194477ea7c73af00273bc4337ac0e4495691
Christian Acevedo <cacesa11@gmail.com>
2022-10-27 00:25:32 -0500

    cambio2

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -4,7 +4,7 @@ import Logo from '../Images/Logo.png'
 
 function Nav() {
   return (
-    <div className='min-h-[10%] bg-slate-800 flex px-6 py-6 justify-between items-center '>
+    <div className='min-h-[10%] sticky bg-slate-800 flex px-6 py-6 justify-between items-center '>
             <img className='w-[130px] rounded-full' src={Logo} alt=''/> 
             <img className=' lg:hidden  md:hidden  sm:block' src={Menu} alt=''/>     
     </div>
