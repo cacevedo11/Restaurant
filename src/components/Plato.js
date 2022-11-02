be3bbdfbaabc9f5807a4ef1fc95e097b5b9215cb
Christian Acevedo <cacesa11@gmail.com>
2022-11-01 23:43:11 -0500

    cursorpointer

--- src/components/Plato.js
+++ src/components/Plato.js
@@ -16,9 +16,9 @@ function Plato({title, text, price,}) {
         <div className='flex flex-col justify-evenly items-center space-y-0 '>
           <span className='rounded-full bg-white drop-shadow-md shadow-black px-5 py-1'><strong>{price}</strong></span>
           <div className='rounded-full bg-white drop-shadow-md shadow-black px-1'>
-            <button className='px-3 bg-white-500 hover:rounded-full active:rounded-full hover:bg-white-600 active:bg-gray-300 focus:outline-none focus:ring focus:ring-violet-300'>-</button>
-            <span className='px-1'> 0 </span>
-            <button className='px-3 bg-white-500 hover:rounded-full active:rounded-full hover:bg-white-600 active:bg-gray-300 focus:outline-none focus:ring focus:ring-violet-300'>+</button>
+            <button className='cursor-pointer px-3 bg-white-500 hover:rounded-full active:rounded-full hover:bg-white-600 active:bg-gray-300 focus:outline-none focus:ring focus:ring-gray-300'>-</button>
+            <span className='px-1 cursor-pointer'> 0 </span>
+            <button className='cursor-pointer px-3 bg-white-500 hover:rounded-full active:rounded-full hover:bg-white-600 active:bg-gray-300 focus:outline-none focus:ring focus:ring-gray-300'>+</button>
           </div>
         </div>
 
